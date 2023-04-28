b758b3c3e8b4fa74618860745d4408081c7f0ad3
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-04-28 15:51:45 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const inputEl = document.querySelector('input[name="searchQuery"]');
 const btnEl = document.querySelector('button[type="submit"]');
 const galleryEl = document.querySelector('.gallery');
+const bottomBarEl = document.querySelector('.bottom-bar');
+const LoadBtnEl = document.querySelector('.load-more');
 
 const API_URL = 'https://pixabay.com/api/?';
 const API_KEY = '35750210-01538b5c80567ccad47fd3a82';
@@ -15,18 +18,26 @@ const fetchPhotosData = async () => {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page: page,
+      per_page: 40,
     },
   });
 };
 
 const createGallery = () => {
+  page = 1;
   fetchPhotosData()
     .then(response => {
-      if (response && inputEl.value !== '') {
+      if (response.data.hits.length > 0 && inputEl.value !== '') {
         clear();
         createMarkup(response);
+
+        Notiflix.Notify.success(
+          'Hooray! We found ' + response.data.totalHits + ' images.'
+        );
+        bottomBarEl.classList.remove('hidden');
       } else {
-        console.log(
+        Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
@@ -46,19 +57,19 @@ const createMarkup = response => {
   const markup = response.data.hits
     .map(hit => {
       return `<div class="photo-card">
-        <img src="${hit.previewURL}" alt="${hit.tags}" loading="lazy" />
+        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
         <div class="info">
-          <p class="info-item">
-            <b>${hit.likes}</b>
+          <p class="info-item"><b>Likes</b>
+            ${hit.likes}
           </p>
-          <p class="info-item">
-            <b>${hit.views}</b>
+          <p class="info-item"><b>Views</b>
+            ${hit.views}
           </p>
-          <p class="info-item">
-            <b>${hit.comments}</b>
+          <p class="info-item"><b>Comments</b>
+            ${hit.comments}
           </p>
-          <p class="info-item">
-            <b>${hit.downloads}</b>
+          <p class="info-item"><b>Downloads</b>
+            ${hit.downloads}
           </p>
         </div>
       </div>`;
@@ -67,3 +78,15 @@ const createMarkup = response => {
 
   galleryEl.innerHTML = markup;
 };
+
+const loadMore = () => {
+  page++;
+  fetchPhotosData().then(response => {
+    galleryEl.insertAdjacentHTML('beforeend', createMarkup(response));
+  });
+};
+
+LoadBtnEl.addEventListener('click', event => {
+  event.preventDefault();
+  loadMore();
+});
